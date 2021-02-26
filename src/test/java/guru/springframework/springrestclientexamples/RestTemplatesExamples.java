3ea2ac699721113c057c577371a9a7f408b6a12b
Fredohm <fredwaucquez@hotmail.fr>
2021-02-26 17:19:27 +0100

    370. rest Template Examples Part 2.

--- src/test/java/guru/springframework/springrestclientexamples/RestTemplatesExamples.java
+++ src/test/java/guru/springframework/springrestclientexamples/RestTemplatesExamples.java
@@ -2,10 +2,15 @@ package guru.springframework.springrestclientexamples;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -85,6 +90,7 @@ public class RestTemplatesExamples {
 
         JsonNode updateNode = restTemplate.getForObject(apiUrl + id, JsonNode.class);
 
+        assert updateNode != null;
         System.out.println(updateNode.toString());
 
 
@@ -92,16 +98,105 @@ public class RestTemplatesExamples {
 
     @Test(expected = ResourceAccessException.class)
     public void updateCustomerUsingPatchSunHHttp() throws Exception {
+        String apiUrl = API_ROOT + "/customers/";
 
+        RestTemplate restTemplate = new RestTemplate();
+
+        Map<String, Object> postMap = new HashMap<>();
+        postMap.put("firstname", "Sam");
+        postMap.put("lastname", "Axe");
+
+        JsonNode jsonNode = restTemplate.postForObject(apiUrl, postMap, JsonNode.class);
+
+        System.out.println("Response");
+        assert jsonNode != null;
+        System.out.println(jsonNode.toString());
+
+        String customerUrl = jsonNode.get("customer_url").textValue();
+
+        String id = customerUrl.split("/")[3];
+
+        System.out.println("Created customer id :" + id);
+
+        postMap.put("firstname", "Sammy");
+        postMap.put("lastname", "Axed");
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(postMap, httpHeaders);
+
+        JsonNode updateNode = restTemplate.patchForObject(apiUrl + id, entity, JsonNode.class);
+
+        assert updateNode != null;
+        System.out.println(updateNode.toString());
     }
 
     @Test
     public void updateCustomerUsingPatch() throws Exception {
+        String apiUrl = API_ROOT + "/customers/";
+
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
+
+        Map<String, Object> postMap = new HashMap<>();
+        postMap.put("firstname", "Sam");
+        postMap.put("lastname", "Axe");
+
+        JsonNode jsonNode = restTemplate.postForObject(apiUrl, postMap, JsonNode.class);
+
+        System.out.println("Response");
+        assert jsonNode != null;
+        System.out.println(jsonNode.toString());
+
+        String customerUrl = jsonNode.get("customer_url").textValue();
+
+        String id = customerUrl.split("/")[3];
+
+        System.out.println("Created customer id :" + id);
+
+        postMap.put("firstname", "Sammy");
+        postMap.put("lastname", "Axed");
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(postMap, httpHeaders);
+
+        JsonNode updateNode = restTemplate.patchForObject(apiUrl + id, entity, JsonNode.class);
+
+        assert updateNode != null;
+        System.out.println(updateNode.toString());
 
     }
 
     @Test(expected = HttpClientErrorException.class)
     public void deleteCustomer() throws Exception {
+        String apiUrl = API_ROOT + "/customers/";
 
+        RestTemplate restTemplate = new RestTemplate();
+
+        Map<String, Object> postMap = new HashMap<>();
+        postMap.put("firstname", "Les");
+        postMap.put("lastname", "Claypool");
+
+        JsonNode jsonNode = restTemplate.postForObject(apiUrl, postMap, JsonNode.class);
+
+        System.out.println("Response");
+        assert jsonNode != null;
+        System.out.println(jsonNode.toString());
+
+        String customerUrl = jsonNode.get("customer_url").textValue();
+
+        String id = customerUrl.split("/")[3];
+
+        System.out.println("Created customer id :" + id);
+
+        restTemplate.delete(apiUrl + id);
+        System.out.println("Customer deleted");
+
+        restTemplate.getForObject(apiUrl + id, JsonNode.class);
     }
 }
